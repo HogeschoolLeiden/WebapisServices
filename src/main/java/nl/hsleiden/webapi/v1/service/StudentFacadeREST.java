66731b6026e39140a89c23a2802cfd62cc9c3792
Jacqueline <kok.j@hsleiden.nl>
2013-08-01 13:49:49 +0200

    removed unused imports

--- src/main/java/nl/hsleiden/webapi/v1/service/StudentFacadeREST.java
+++ src/main/java/nl/hsleiden/webapi/v1/service/StudentFacadeREST.java
@@ -4,7 +4,6 @@
  */
 package nl.hsleiden.webapi.v1.service;
 
-import com.sun.jersey.api.NotFoundException;
 import java.net.URI;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -19,10 +18,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import nl.hsleiden.webapi.exception.BadRequestError;
